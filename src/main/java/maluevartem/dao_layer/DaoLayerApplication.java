68a2d2e4647082a20fc1fb90f993441a15022387
ArtemMaluev <artem.rus13@yandex.ru>
2022-10-02 20:12:19 +0300

    Initial commit

---
+++ src/main/java/maluevartem/dao_layer/DaoLayerApplication.java
@@ -0,0 +1,12 @@
+package maluevartem.dao_layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoLayerApplication.class, args);
+    }
+}
